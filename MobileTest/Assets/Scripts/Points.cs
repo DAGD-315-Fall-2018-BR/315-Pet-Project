040e28791f97c4c740ac56975af2125f0efaa815
ptownazkikr <44704433+ptownazkikr@users.noreply.github.com>
2018-11-30 10:45:12 -0500

    Added Points and Dog Collar Mechanics

---
+++ MobileTest/Assets/Scripts/Points.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Points : MonoBehaviour {
+
+	private float score = 0.0f;
+	private bool hasPointMult = false;
+	private float timer = 0.0f;
+	private float timerBase = 5.0f;
+	private int pointMult = 1;
+	private int basePointMult = 1;
+
+	UnityAction listener;
+	private void Awake()
+	{
+		listener = new UnityAction(PointMultPowerup);
+	}
+	private void OnEnable()
+	{
+		EventManager.StartListening("pointMultPowerup", listener);
+	}
+
+	private void OnDisable()
+	{
+		EventManager.StopListening("pointMultPowerup", listener);
+	}
+
+
+
+
+	void PointMultPowerup()
+	{
+		Debug.Log("MORE POINTS!");
+		pointMult = 2;
+		hasPointMult = true;
+		timer = 0;
+	}
+
+	void Update()
+	{
+		score += Time.deltaTime * pointMult;
+		//Debug.Log(Mathf.RoundToInt(score));
+
+		if (hasPointMult)
+		{
+			timer += Time.deltaTime;
+			if (timer > timerBase)
+			{
+				Debug.Log("Powerup Done");
+				pointMult = basePointMult;
+				hasPointMult = false;
+				timer = 0;
+			}
+		}
+	}
+}
